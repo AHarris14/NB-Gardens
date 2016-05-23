2d51b6998cb499353a19c640f37c8f27e51ab423
bstinchcombe <bs305@exeter.ac.uk>
2016-05-23 15:00:20 +0000

    addresses

---
+++ nb-gardens/Database/Scripts/Data/address.sql
@@ -0,0 +1,6 @@
+INSERT INTO address (addressLine1, addressLine2, postCode, town, county, country) VALUES ('69 Urmums Avenue', '','QW1 2ER','London', '','UK');
+INSERT INTO address (addressLine1, addressLine2, postCode, town, county, country) VALUES ('1 Some Place', 'Some Plaice', 'TY34 56UI','Reading', '', 'UK');
+INSERT INTO address (addressLine1, addressLine2, postCode, town, county, country) VALUES ('56a Hole Street', '', 'OP7 8AS','Manchester','','UK');
+INSERT INTO address (addressLine1, addressLine2, postCode, town, county, country) VALUES ('22 Styff Crescent', '','DF9 0GH','Cardiff', 'South Wales','UK');
+INSERT INTO address (addressLine1, addressLine2, postCode, town, county, country) VALUES ('342 Ill Lane', '','JK1 2DI','Edinburgh', 'Scotland','UK');
+INSERT INTO address (addressLine1, addressLine2, postCode, town, county, country) VALUES ( '13 Open Close', 'Theodor Industrial Estate', 'LZ34 5XC', 'Paris', '', 'France');
