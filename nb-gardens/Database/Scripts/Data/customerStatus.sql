99df1746b96765b8c0aa44e0b47dff08d1927a6f
bstinchcombe <bs305@exeter.ac.uk>
2016-05-23 15:58:15 +0000

    customerStatus stuff

---
+++ nb-gardens/Database/Scripts/Data/customerStatus.sql
@@ -0,0 +1,5 @@
+INSERT INTO customerstatus (`status`) VALUES ('Blocked');
+INSERT INTO customerstatus (`status`) VALUES ('Pending');
+INSERT INTO customerstatus (`status`) VALUES ('Active');
+INSERT INTO customerstatus (`status`) VALUES ('On Hold');
+INSERT INTO customerstatus (`status`) VALUES ('Flagged');
