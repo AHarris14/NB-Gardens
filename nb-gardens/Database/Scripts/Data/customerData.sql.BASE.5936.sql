f3de9acdbe8416ee7c1386e05006336c12b93158
sallen1 <sam93allen@hotmail.com>
2016-05-23 15:47:25 +0000

    test

--- nb-gardens/Database/Scripts/Data/customerData.sql.BASE.5936.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO `nbgardens`.`customer` (`firstName`, `lastName`, `DOB`, `email`, `creditRemaining`, `phoneNumber`, `dateCreated`, `dateModified`, `customerstatus_statusId`, `Address_addressId`, `username`, `password`)
-VALUES 
-('Georgei', 'Peyton', '02/06/1939', 'goodolddays1939@gmail.co.uk', '8921.89', '07975201315', '23/05/2016', '23/05/2016', '3', '1', 'olddays', '2e71777dff35158b955605a606c042e4ad0b3217'),
-('Kim', 'Harlow', '29/12/1943', 'sweetsweetroyalty@gmail.com', '8991.98', '07697135336', '23/05/2016', '23/05/2016', '3', '2', 'royal1', '4dbc8c31da4ffd6e039f26f7a2707d0cd15fc563'),
-('Terrence', 'Cooke', '06/11/1944', 'lobsterbisque@aol.com', '1786.56', '07850661978', '23/05/2016', '23/05/2016', '3', '3', 'bisqueisgreat11', '9a86c2a4fd81e21711e5c9ce7e03995ead892a7b'),
-('Matthew', 'Hameldon', '17/01/1951', 'gloryhameldon@btinternet.com', '6534.98', '07089532432', '23/05/2016', '23/05/2016', '3', '4', 'morninggloryhameldon', '3e581f77c81e8f6bf3b98d7823f6393a01d1b880'),
-('Wigheard Boniface', 'Everett', '11/09/1955', 'hearmywig@aol.co.uk', '1675.91', '07298820624', '23/05/2016', '23/05/2016', '3', '5', 'wig42', 'fa7a3966d55d6b7cc7fd89709b69e8fd82d0423e'),
-('Aharon Nogah', 'Babcock', '18/02/1970', 'anogah@.gmail.co.uk', '0', '07462704357', '23/05/2016', '23/05/2016', '5', '6', 'aharon1970', 'c2072a50342594eb27cee996d3bf5239067bd3b5');
