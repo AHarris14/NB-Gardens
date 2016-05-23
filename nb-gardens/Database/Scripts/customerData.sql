adaffebf71c59d419d88417aa1f7dd374bdee63c
dtgreen <dtgreen.work@gmail.com>
2016-05-23 10:42:44 +0000

    DG - Updating files

---
+++ nb-gardens/Database/Scripts/customerData.sql
@@ -0,0 +1,5 @@
+INSERT INTO `nbgardens`.`customer` (`firstName`, `lastName`, `DOB`, `email`, `credit`, `status`, `phoneNumber`, `dateCreated`, `dateModified`, `customerstatus_statusId`, `Address_addressId`, `username`, `password`)
+VALUES 
+(NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+(NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+(NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
