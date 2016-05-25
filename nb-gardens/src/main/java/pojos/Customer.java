3b905b6edf540bb85c26bd5ba5749a5cc5a48979
dtgreen <dtgreen.work@gmail.com>
2016-05-25 11:39:08 +0000

    pojo

--- nb-gardens/src/main/java/pojos/Customer.java
+++ nb-gardens/src/main/java/pojos/Customer.java
@@ -1,6 +1,14 @@
 package pojos;
+import java.sql.Date;
 
 public class Customer
 {
+	private int customerID;
+	private String firstName;
+	private String lastName;
+	private java.util.Date dateOfBirth;
+	private String email;
+	private double creditRemaining;
+	private String phoneNumber;
 	
 }
